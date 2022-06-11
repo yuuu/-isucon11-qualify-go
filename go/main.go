be2755f4a0c204ab828c581732a60af210884d3a
yuuu <okazaki@fusic.co.jp>
2022-06-12 06:16:21 +0900

    getIsuGraphの取得レコード数を絞る

--- go/main.go
+++ go/main.go
@@ -787,7 +787,10 @@ func generateIsuGraphResponse(tx *sqlx.Tx, jiaIsuUUID string, graphDate time.Tim
 	var startTimeInThisHour time.Time
 	var condition IsuCondition
 
-	rows, err := tx.Queryx("SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = ? ORDER BY `timestamp` ASC", jiaIsuUUID)
+	rows, err := tx.Queryx(
+		"SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = ? AND `timestamp` >= ? AND `timestamp` < ? ORDER BY `timestamp` ASC",
+		jiaIsuUUID, graphDate, graphDate.Add(time.Hour*24),
+	)
 	if err != nil {
 		return nil, fmt.Errorf("db error: %v", err)
 	}
